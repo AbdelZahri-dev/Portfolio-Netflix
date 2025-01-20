addd9d7c15234b266b3c1e9cabcf55630a72604d
AbdelZahri-dev <Zahri.abdel@gmail.com>
2025-01-20 13:35:41 +0100

    remplacement BrowserRouter par HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+
+// ici on remplace BrowserRouter par HashRouter pour que le site fonctionne sur Github 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 
   </StrictMode>,
 )
